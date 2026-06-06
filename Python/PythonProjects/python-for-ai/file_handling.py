a51245a1f1790dd04c4242da02f8ff01c27e254b
SnehaNair3 <snehanairk369@gmail.com>
2026-06-06 22:33:13 +0530

    file-handling files added.

--- Python/PythonProjects/python-for-ai/file_handling.py
+++ Python/PythonProjects/python-for-ai/file_handling.py
@@ -24,7 +24,41 @@ print(file.readline(2))
 file=open('file_handling_test.txt','r')
 print(file.readline(50)) 
 
+# Writing  a file
+file=open('file_handling_test.txt','w')
+file.write("Iam from Kerala and i live in Bangalore now.")
+file.write("I love reading books, solving puzzles,studying etc")
+file.write("I wanna learn tennis, dance, badminton, swimming, content creation.")
+file.close()
+file=open('file_handling_test.txt','r')
+print(file.read())
 
+
+# Append operation
+with open('file_handling_test.txt','a') as file:
+    file.write("I work in the computer science domain specifically focusing on ai-ml and data science.")
+file=open('file_handling_test.txt','r')
+for line in file:
+    print(line)
+
+# \n operation
+file=open('file_handling_test.txt','w')
+file.write("\n")    
+file.write("My family consists of four members.")    
+file.write("\n")    
+file.write("I love my family.")    
+file.close()
+file=open('file_handling_test.txt','r')
+print(file.read())
+
+
+with open('file_handling_test.txt','a+') as file:
+    file.write("\n")
+    file.write("I love Kerala, its such a beautiful place.")
+file=open('file_handling_test.txt','r')
+for line in file:
+    print(line)   
+  
 
   
 
