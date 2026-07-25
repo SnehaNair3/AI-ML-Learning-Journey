74f7de00f075d4557863c9e7ac8da8e50baeaa48
SnehaNair3 <snehanairk369@gmail.com>
2026-07-25 11:54:57 +0530

    DML files added.

--- SQL for Data Science/DML_example.sql
+++ SQL for Data Science/DML_example.sql
@@ -18,6 +18,9 @@ INSERT INTO emp_exercise VALUES ('Dirk Smith','Programmer ||',45,75020.00);
 INSERT INTO emp_exercise VALUES ('Federick Manuel','Programmer ||',56,69000.00);
 INSERT INTO emp_exercise VALUES ('Abin Philipose','Programmer',25,96500.00);
 INSERT INTO emp_exercise VALUES ('John Jacob','Doctor',36,95000.00);
+INSERT INTO emp_exercise VALUES ('Alan Turing','Secretary',38,39500.00);
+INSERT INTO emp_exercise VALUES ('Joyal Dojo','Secretary',42,35500.00);
+INSERT INTO emp_exercise VALUES ('Merin Jose','IT Support',31,45000.00);
 
 
 SELECT * FROM emp_exercise;
@@ -40,4 +43,38 @@ SELECT * FROM emp_exercise WHERE emp_name LIKE '%ITH';
 # UPDATE
 SET SQL_SAFE_UPDATES=0;
 UPDATE emp_exercise SET designation='Senior Programmer' WHERE emp_name ='Dirk Smith';
+
+UPDATE emp_exercise SET designation='Data Analyst',emp_age=27 WHERE emp_name='Abin Philipose';
+
+UPDATE emp_exercise SET designation='Data Analyst' WHERE emp_name='Jonie Weber' and emp_age=28;
+
+UPDATE emp_exercise SET emp_name='Jonie Williams' WHERE emp_name='Jonie Weber';
+SELECT * FROM emp_exercise;
+
+UPDATE emp_exercise SET emp_age=emp_age+1 WHERE emp_name='Dirk Smith';
+SELECT * FROM emp_exercise;
+
+UPDATE emp_exercise SET designation='Administrative Assistant' WHERE designation='Secretary';
+SELECT * FROM emp_exercise;
+
+
+UPDATE emp_exercise SET emp_salary=emp_salary+3500 WHERE emp_salary<30000;
+SELECT * FROM emp_exercise;
+
+
+UPDATE emp_exercise SET emp_salary=emp_salary+4500 WHERE emp_salary >33500;
+SELECT * FROM emp_exercise;
+
+
+UPDATE emp_exercise SET designation='Programmer |||' WHERE designation='Programmer ||';
+SELECT * FROM emp_exercise;
+
+
+UPDATE emp_exercise SET designation='Programmer ||' WHERE designation='Programmer';
+SELECT * FROM emp_exercise;
+
+
+DELETE FROM emp_exercise WHERE emp_name='Merin Jose' AND designation='IT Support';
+SELECT * FROM emp_exercise;
+
 
