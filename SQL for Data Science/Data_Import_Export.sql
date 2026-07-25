06dc7ac755aa68900790b3c6ad5885f8e3f3539a
SnehaNair3 <snehanairk369@gmail.com>
2026-07-25 17:02:51 +0530

    data import files added.

---
+++ SQL for Data Science/Data_Import_Export.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE data_import;
+
+USE data_import;
+
+CREATE TABLE air_passenger
+(
+Month VARCHAR(10),
+Passengers INT
+);
+
+DROP  TABLE air_passenger;
+
+SELECT * FROM air_passenger;
